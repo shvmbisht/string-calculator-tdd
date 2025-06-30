7eba6b1048f47ed196d3dd7e7f3ed3ee016d3a1c
Shivam Bisht <shvmbisht@gmail.com>
2025-06-30 17:41:28 +0530

    feat: 6. Support custom delimiters

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,9 +1,17 @@
 module.exports = {
   add(numbers) {
-    if (numbers === '') {
-      return 0;
+    if (numbers === '') return 0;
+
+    let delimiter = /,|\n/;
+    let nums = numbers;
+
+    if (numbers.startsWith('//')) {
+      const parts = numbers.split('\n');
+      const delimiterPart = parts[0];
+      delimiter = delimiterPart.substring(2);
+      nums = parts[1];
     }
-    const nums = numbers.replace(/\n/g, ',');
-    return nums.split(',').reduce((sum, n) => sum + parseInt(n, 10), 0);
+
+    return nums.split(delimiter).reduce((sum, n) => sum + parseInt(n, 10), 0);
   },
 }; 
