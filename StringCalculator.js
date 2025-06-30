05ef00cfb361196a8781ed7ad7d7ea0da7522793
Shivam Bisht <shvmbisht@gmail.com>
2025-06-30 17:37:41 +0530

    feat: 1. Handle empty string

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,20 +1,7 @@
 module.exports = {
   add(numbers) {
-    if (numbers === '') return 0;
-    let delimiter = /,|\n/;
-    let nums = numbers;
-    if (numbers.startsWith('//')) {
-      const match = numbers.match(/^\/\/(.)\n(.*)$/s);
-      if (match) {
-        delimiter = new RegExp(match[1]);
-        nums = match[2];
-      }
+    if (numbers === '') {
+      return 0;
     }
-    const numList = nums.split(delimiter).map(n => parseInt(n, 10));
-    const negatives = numList.filter(n => n < 0);
-    if (negatives.length > 0) {
-      throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
-    }
-    return numList.reduce((sum, n) => sum + n, 0);
   },
 }; 
