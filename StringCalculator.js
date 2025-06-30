11358ce45139026325ee0eb150d76359a61e0aea
Shivam Bisht <shvmbisht@gmail.com>
2025-06-30 17:42:54 +0530

    feat: 7. Throw exception on negative numbers

--- StringCalculator.js
+++ StringCalculator.js
@@ -3,15 +3,22 @@ module.exports = {
     if (numbers === '') return 0;
 
     let delimiter = /,|\n/;
-    let nums = numbers;
+    let numsStr = numbers;
 
     if (numbers.startsWith('//')) {
       const parts = numbers.split('\n');
       const delimiterPart = parts[0];
       delimiter = delimiterPart.substring(2);
-      nums = parts[1];
+      numsStr = parts[1];
     }
 
-    return nums.split(delimiter).reduce((sum, n) => sum + parseInt(n, 10), 0);
+    const numList = numsStr.split(delimiter).map(n => parseInt(n, 10));
+    const negatives = numList.filter(n => n < 0);
+
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
+    }
+
+    return numList.reduce((sum, n) => sum + n, 0);
   },
 }; 
